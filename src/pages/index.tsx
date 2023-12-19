85ea014c03d9f67582122e198d03491d01d808ab
K-Y-Hoo <bookopen1@gmail.com>
2023-12-19 17:12:23 +0900

    [#23] feat: 랜딩 페이지 초기 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
 import { Inter } from 'next/font/google';
-
+import S from '@/styles/Home.module.css';
+import Image from 'next/image';
+import HomeImg from '@/assets/images/Home1.png';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      {/* <header></header> */}
+      <main className={S.main}>
+        <div className={S.article}>
+          <div className={S.imgContainer}>
+            <Image src={HomeImg} alt="홈이미지1" objectFit="cover" />
+          </div>
+
+          <h1 className={S.h1}>새로운 일정 관리</h1>
+          <br className={S.br} />
+          <h1 className={S.h1Violet}>Taskify</h1>
+          <p>서비스에 대한 설명</p>
+          {/* <button></button> */}
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
+      <footer></footer>
+    </>
+  );
 }
