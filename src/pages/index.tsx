1fb72967006ef754ad27ffa50eb38f1e33fd88ff
K-Y-Hoo <bookopen1@gmail.com>
2023-12-20 11:21:30 +0900

    [#23] feat: 랜딩 페이지 포인트1 섹션 반응형 디자인 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import { Inter } from 'next/font/google';
 import S from './index.module.css';
 import Image from 'next/image';
 import HomeImg from '@/assets/images/Home1.png';
+import HomeImg2 from '@/assets/images/Home2.png';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       {/* <header></header> */}
-      <main className={S.main}>
+      <body className={S.body}>
         <div className={S.article}>
           <div className={S.imgContainer}>
             <Image src={HomeImg} alt="홈이미지1" fill={true} />
@@ -21,11 +22,23 @@ export default function Home() {
           {/* <button></button> */}
         </div>
         <div className={S.pointContainer}>
-          <div className={S.point}>1</div>
+          <div className={S.point}>
+            <div className={S.pointHeader}>
+              <p className={S.pointH1}>Point 1</p>
+              <p className={S.pointDescription}>
+                일의 <span className={S.pointDescriptionSpan}>우선순위를</span>{' '}
+                <br />
+                관리하세요
+              </p>
+            </div>
+            <div className={S.pointImgContainer1}>
+              <Image className={S.pointImg1} src={HomeImg2} alt="홈이미지2" />
+            </div>
+          </div>
           <div className={S.point}>2</div>
           <div></div>
         </div>
-      </main>
+      </body>
       <footer></footer>
     </>
   );
