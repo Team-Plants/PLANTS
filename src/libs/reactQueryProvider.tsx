a4fc7c0c485e090f985e9e94da846e55786ce892
전수빈 <soobin08011812@gmail.com>
2023-12-20 09:27:24 +0900

    [#6] feat: QueryClient defaultOptions 추가 및 eslintrc extends 추가

--- src/libs/reactQueryProvider.tsx
+++ src/libs/reactQueryProvider.tsx
@@ -3,7 +3,16 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 function ReactQueryProviders({ children }: React.PropsWithChildren) {
-  const [client] = useState(() => new QueryClient());
+  const [client] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={client}>
