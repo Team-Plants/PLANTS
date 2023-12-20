591765c6b9d6228a87ec2c3688ae8eed4d654680
hyejungan <138510303+hyejungan@users.noreply.github.com>
2023-12-21 00:10:18 +0900

    [#26] fix : index 필요없는 부분 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import EmailInput from '@/components/Input/EmailInput';
-import PasswordInput from '@/components/Input/PasswordInput';
-import PasswordCheckInput from '@/components/Input/passwordCheck';
-
 export default function Home() {
-  return (
-    <>
-      <EmailInput/>
-      <PasswordInput />
-      <PasswordCheckInput />
-    </>
-  );
+  return <></>;
 }
