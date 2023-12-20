b3a60865feac270943f056c9cdf3e017f5c933d9
BeMatthewsong <songww1997@naver.com>
2023-12-20 15:53:41 +0900

    [#25] chore: 메인브랜치 내용 반영

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
-import { Inter } from 'next/font/google';
-import LightHeader from '@/components/header/LightHeader';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <LightHeader></LightHeader>;
+  return <></>;
 }
