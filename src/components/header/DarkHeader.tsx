49f13616492c75a74fa5ed89eda3894ec8625e36
BeMatthewsong <songww1997@naver.com>
2023-12-21 13:24:13 +0900

    [#25] fix: 이미지 네이밍, css 파일 1개로 통일, px로 수정

--- src/components/header/DarkHeader.tsx
+++ src/components/header/DarkHeader.tsx
@@ -1,24 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Logo from '@/assets/icons/WhiteLogoWithLetter.svg';
-import SmallLogo from '@/assets/icons/WhiteSmallLogo.svg';
-import S from '@/components/header/DarkHeader.module.css';
+import LogoImg from '@/assets/icons/WhiteLogoWithLetter.svg';
+import SmallLogoImg from '@/assets/icons/WhiteSmallLogo.svg';
+import S from '@/components/header/header.module.css';
 
 function DarkHeader() {
   return (
-    <div className={S.headerContainer}>
+    <div className={`${S.headerContainer} ${S.darkBg}`}>
       <div className={S.logo}>
         <Image
-          src={Logo}
+          src={LogoImg}
           alt="이미지와 폰트가 있는 로고"
           width={121}
           height={39}
         />
       </div>
       <div className={S.smallLogo}>
-        <Image src={SmallLogo} alt="이미지 로고 " width={23} height={27} />
+        <Image src={SmallLogoImg} alt="이미지 로고 " width={23} height={27} />
       </div>
-      <div className={S.links}>
+      <div className={`${S.links} ${S.darkBg}`}>
         <Link href="/signin">로그인</Link>
         <Link href="/signup">회원가입</Link>
       </div>
--- src/components/header/LightHeader.tsx
+++ src/components/header/LightHeader.tsx
@@ -1,24 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Logo from '@/assets/icons/logoWithLetter.svg';
-import SmallLogo from '@/assets/icons/SmallLogo.svg';
-import S from '@/components/header/LightHeader.module.css';
+import LogoImg from '@/assets/icons/logoWithLetter.svg';
+import SmallLogoImg from '@/assets/icons/SmallLogo.svg';
+import S from '@/components/header/header.module.css';
 
 function LightHeader() {
   return (
-    <div className={S.headerContainer}>
+    <div className={`${S.headerContainer} ${S.lightBg}`}>
       <div className={S.logo}>
         <Image
-          src={Logo}
+          src={LogoImg}
           alt="이미지와 폰트가 있는 로고"
           width={121}
           height={39}
         />
       </div>
       <div className={S.smallLogo}>
-        <Image src={SmallLogo} alt="이미지 로고 " width={23} height={27} />
+        <Image src={SmallLogoImg} alt="이미지 로고 " width={23} height={27} />
       </div>
-      <div className={S.links}>
+      <div className={`${S.links} ${S.lightBg}`}>
         <Link href="/signin">로그인</Link>
         <Link href="/signup">회원가입</Link>
       </div>
