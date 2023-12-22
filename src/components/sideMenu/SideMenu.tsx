7ffc8713c5fd9f66f5d53c27457f16bdc7234e5f
K-Y-Hoo <bookopen1@gmail.com>
2023-12-22 23:33:24 +0900

    [#36] feat: 무한 스크롤 기능 최종 구현

--- src/components/sideMenu/SideMenu.tsx
+++ src/components/sideMenu/SideMenu.tsx
@@ -17,25 +17,39 @@ function SideMenu({ pageId }: SideMenuProps) {
   const [dashboards, setDashboards] = useState<DashBoardList[]>([]);
   const [target, setTarget] = useState<HTMLDivElement | null>(null);
   const [cursorId, setCursorId] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState<number>(Infinity);
+  const [currentLength, setCurrentLength] = useState(0);
 
   async function fetchMoreDashboards() {
-    const response: AxiosResponse = await axios.get(
-      'https://sp-taskify-api.vercel.app/5/dashboards',
-      {
-        params: {
-          navigationMethod: 'infiniteScroll',
-          size: 10,
-          cursorId: cursorId,
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      const response: AxiosResponse = await axios.get(
+        'https://sp-taskify-api.vercel.app/5/dashboards',
+        {
+          params: {
+            navigationMethod: 'infiniteScroll',
+            size: 10,
+            cursorId: cursorId,
+          },
+          headers: {
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjYsInRlYW1JZCI6IjUiLCJpYXQiOjE3MDMxNTM0MjEsImlzcyI6InNwLXRhc2tpZnkifQ.EymSG57SnaoeMZQ79mPVpzMbk8FB7Vyr_Hb0P_yFZvY',
+          },
         },
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjYsInRlYW1JZCI6IjUiLCJpYXQiOjE3MDMxNTM0MjEsImlzcyI6InNwLXRhc2tpZnkifQ.EymSG57SnaoeMZQ79mPVpzMbk8FB7Vyr_Hb0P_yFZvY',
-        },
-      },
-    );
-    const data = await response?.data;
-    setCursorId(data.cursorId + 8);
-    setDashboards((prev) => [...prev, ...data.dashboards]);
+      );
+      const data = await response?.data;
+      setCursorId(data.cursorId + 8);
+      setDashboards((prev) => [...prev, ...data.dashboards]);
+      setCurrentLength((prev) => prev + data.dashboards.length);
+      setTotalCount(data.totalCount);
+    } catch (error) {
+      console.error('데이터를 불러오는 중 에러가 발생했습니다:', error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -48,7 +62,8 @@ function SideMenu({ pageId }: SideMenuProps) {
         if (entry.isIntersecting) {
           observer.unobserve(entry.target);
           await fetchMoreDashboards();
-          observer.observe(entry.target);
+        } else {
+          observer.observe(target);
         }
       };
       observer = new IntersectionObserver(onIntersect, { threshold: 1 });
@@ -122,7 +137,10 @@ function SideMenu({ pageId }: SideMenuProps) {
               </Link>
             ))}
           <div ref={setTarget} className={S.refContainer}>
-            Loading...
+            {isLoading && 'loading...'}
+          </div>
+          <div className={S.warning}>
+            {totalCount === currentLength ? 'no more data!' : null}
           </div>
         </ul>
       </div>
