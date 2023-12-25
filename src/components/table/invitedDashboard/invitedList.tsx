2075d99fa790e4cb44b2ed132118081218bf8128
BeMatthewsong <songww1997@naver.com>
2023-12-25 22:47:57 +0900

    [#33] feat: 초대받은 대시보드 컴포넌트 추가

--- src/components/table/invitedDashboard/invitedList.tsx
+++ src/components/table/invitedDashboard/invitedList.tsx
@@ -1,28 +1,50 @@
-interface tableListProps {
-  name: string;
-  label: string;
-  tableData: string; // data[]
-  pagination?: string; // 임시
-  inviteButton?: string; // 임시
+import S from '@/components/table/invitedDashboard/invitedList.module.css';
+import SearchBar from '@/components/search/searchBar';
+import InvitedItem from './invitedItem';
+
+interface InvitedListProps {
+  id: number;
+  inviterUserId: number;
+  teamId: number;
+  dashboard: {
+    title: string;
+    id: number;
+  };
+  invitee: {
+    nickname: string;
+    email: string;
+    id: number;
+  };
+  inviteAccepted: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
 
-function tableList({
-  name,
-  label,
-  tableData,
-  pagination,
-  inviteButton,
-}: tableListProps) {
+function InvitedList({ invitations }: { invitations: InvitedListProps[] }) {
   return (
-    <>
-      <div>
-        {name}, {pagination}, {inviteButton}
+    <div className={S.container}>
+      <div className={S.header}>초대받은 대시보드</div>
+      <div className={S.searchBar}>
+        <SearchBar />
       </div>
-      <div>{label}</div>
-      <div>{tableData}</div>
-      {/* 첫번째랑 마지막 child만 스타일 조정 */}
-    </>
+      <div className={S.label}>
+        <div>이름</div> <div>초대자</div> <div>수락 여부</div>
+      </div>
+      <div>
+        {invitations &&
+          invitations.map((invitation) => {
+            return (
+              <div key={invitation.id} className={S.tableItem}>
+                <InvitedItem
+                  dashBoardTitle="프로덕트 디자인"
+                  inviter="손동희"
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
 
-export default tableList;
+export default InvitedList;
--- src/pages/test.tsx
+++
@@ -1,8 +0,0 @@
-import SearchBar from '@/components/search/searchBar';
-import React from 'react';
-
-function test() {
-  return <SearchBar />;
-}
-
-export default test;
