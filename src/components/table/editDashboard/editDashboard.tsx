d05f9837fb3d9895a34c8cd85bc22506b6d8bf92
K-Y-Hoo <bookopen1@gmail.com>
2023-12-26 19:08:33 +0900

    [#51] feat: 대시보드 수정 섹션 ui 최종 구현 및 기능 구현

--- src/components/table/editDashboard/editDashboard.tsx
+++ src/components/table/editDashboard/editDashboard.tsx
@@ -1,14 +1,40 @@
+import ColorChipInput from '@/components/modal/input/colorChipInput/colorChipInput';
+import DefaultInput from '@/components/modal/input/defaultInput/defaultInput';
 import S from '@/components/table/editDashboard/editDashboard.module.css';
+import { FieldValues, useForm } from 'react-hook-form';
 
 function EditDashboard() {
+  const methods = useForm<FieldValues>({
+    mode: 'onChange',
+    defaultValues: {
+      dashboardTitle: '',
+      color: '',
+    },
+  });
+
+  function handleEditDashboard(data: FieldValues) {
+    console.log(data);
+  }
+
+  const { handleSubmit, control, setValue } = methods;
   return (
-    <div className={S.container}>
+    <form className={S.container} onSubmit={handleSubmit(handleEditDashboard)}>
       <div className={S.header}>
-        {'대시보드 타이틀'}
-        <div>{'색 선택란'}</div>
+        <div className={S.headerTitle}>{'대시보드 타이틀'}</div>
+        <div className={S.chipContainer}>
+          <ColorChipInput setValue={setValue} />
+        </div>
       </div>
       <p className={S.p}>대시보드 이름</p>
-    </div>
+      <DefaultInput
+        placeholder="수정할 이름을 입력해 주세요."
+        control={control}
+        name="dashboardName"
+      />
+      <div className={S.buttonContainer}>
+        <button className={S.submitButton}>변경</button>
+      </div>
+    </form>
   );
 }
 
