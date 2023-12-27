7067b7c90aa37ce06116aaad576f690b335fc40a
BeMatthewsong <songww1997@naver.com>
2023-12-27 16:41:00 +0900

    [#61] feat: 대시보드 페이지 초기 구상

--- src/pages/boards/index.tsx
+++ src/pages/boards/index.tsx
@@ -1,16 +1,56 @@
+import AddButton from '@/components/button/add/addButton';
+import Card from '@/components/card/card';
+import CircleChip from '@/components/chip/circle/circleChip';
+import NumberChip from '@/components/chip/number/numberChip';
+import DashboardHeader from '@/components/header/dashboardHeader/dashboardHeader';
 import SideMenu from '@/components/sideMenu/SideMenu';
 import S from '@/pages/boards/boards.module.css';
+import Image from 'next/image';
+import SettingImg from '@/assets/icons/Setting.svg';
 
 function boards() {
   return (
     <div className={S.container}>
-      <div className={S.sideMenu}>
-        <SideMenu pageId={0} />
-      </div>
-      <div className={S.columnContainer}>
-        <div className={S.header}>헤더</div>
-        <div className={S.rowContainer}>
-          <div className={S.toDo}>To Do</div>
+      <SideMenu pageId={0} />
+      <div className={S.main}>
+        <DashboardHeader
+          folder="플랜츠 최고"
+          users={[
+            {
+              letter: '한',
+              color: 'yellow',
+            },
+            {
+              letter: '안',
+              color: 'yellow',
+            },
+            {
+              letter: '전',
+              color: 'yellow',
+            },
+          ]}
+          user={{
+            letter: 'B',
+            name: '민혁',
+            color: 'blue',
+            ownerFolder: {
+              folder: '플랜츠 최고',
+            },
+          }}
+        />
+        <div className={S.mainContainer}>
+          <div className={S.toDo}>
+            <div className={S.infoContainer}>
+              <div className={S.info}>
+                {/* <CircleChip color="purple" isChecked={false} /> */}
+                <span className={S.sectionName}>TO DO</span>
+                <NumberChip num={2} />
+              </div>
+              <Image src={SettingImg} alt="설정 버튼" width={22} height={22} />
+            </div>
+            <AddButton device="mobile" />
+            <Card title="송민혁 천재" date="12월 27일" />
+          </div>
           <div className={S.onProgress}>On Progress</div>
           <div className={S.done}>Done</div>
           <div className={S.addColumn}>새로운 칼럼 추가</div>
