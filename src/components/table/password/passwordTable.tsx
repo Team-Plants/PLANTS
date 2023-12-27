d2dcb8a4767a73384b971fb9040caeccd214441c
hanseulhee <3021062@gmail.com>
2023-12-27 15:36:46 +0900

    [#47] feat: mypage 개발

---
+++ src/components/table/password/passwordTable.tsx
@@ -0,0 +1,20 @@
+import Input from '@/components/Input/Input';
+import Button from '@/components/button/button';
+import S from '@/components/table/password/passwordTable.module.css';
+
+function PasswordTable() {
+  return (
+    <div className={S.container}>
+      <span className={S.title}>비밀번호 변경</span>
+      <Input type="currentPassword" label="현재 비밀번호" />
+      <Input type="newPassword" label="새 비밀번호" />
+      <Input type="newPasswordCheck" label="새 비밀번호 확인" />
+
+      <div className={S.buttonContainer}>
+        <Button content="변경" status="primary" device="tablet" />
+      </div>
+    </div>
+  );
+}
+
+export default PasswordTable;
