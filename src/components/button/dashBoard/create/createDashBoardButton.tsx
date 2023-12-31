751997b0dd759f102963efbd3a1d860f0ed430b4
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-01 07:57:00 +0900

    [#102] fix : error 수정

--- src/components/button/dashBoard/create/createDashBoardButton.tsx
+++ src/components/button/dashBoard/create/createDashBoardButton.tsx
@@ -1,8 +1,7 @@
 import S from '@/components/button/dashBoard/create/createDashBoard.module.css';
 import PlusChip from '@/components/chip/plus/plusChip';
-import { NewDashboardModalProps } from '@/components/modal/newDashboardModal/newDashboardModal';
 
-function CreateDashBoardButton({ onClick }: NewDashboardModalProps) {
+function CreateDashBoardButton({ onClick }: { onClick: () => void }) {
   return (
     <button className={S.container} onClick={onClick}>
       새로운 대시보드
