32517b9076c71f54fbf10050988f80ca7ff31599
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-04 05:56:31 +0900

    [#127] feat : 각 대시보드 클릭시 대시보드페이지로 이동

--- src/components/button/dashBoard/dashBoardButton.tsx
+++ src/components/button/dashBoard/dashBoardButton.tsx
@@ -2,16 +2,27 @@ import ArrowForwardImg from '@/assets/icons/ArrowForward.svg';
 import Crown from '@/assets/icons/Crown.svg';
 import S from '@/components/button/dashBoard/dashBoard.module.css';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 interface Category {
   content: string;
   color: string;
+  dashboardId: number;
   madeByOwner?: boolean;
 }
 
-function DashBoardButton({ content, color, madeByOwner = false }: Category) {
+function DashBoardButton({
+  content,
+  color,
+  dashboardId,
+  madeByOwner = false,
+}: Category) {
+  const router = useRouter();
+
   return (
-    <button className={S.container}>
+    <button
+      className={S.container}
+      onClick={() => router.push(`/dashboard/${dashboardId}`)}>
       <div className={S.contentContainer}>
         <div className={S.circle} style={{ backgroundColor: color }} />
         <div className={S.content}>{content}</div>
