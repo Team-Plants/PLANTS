c3e378a87004c59d8ad7614385fb111c978d54bc
BeMatthewsong <songww1997@naver.com>
2024-01-04 19:12:22 +0900

    [#131] feat: 플러스박스 아이콘 추가

--- src/components/button/invitation/invitation.tsx
+++ src/components/button/invitation/invitation.tsx
@@ -1,6 +1,6 @@
 import S from '@/components/button/invitation/invitation.module.css';
 import { EventHandler, MouseEvent } from 'react';
-import AddButtonImg from '@/assets/icons/AddBox.svg';
+import WhiteAddButtonImg from '@/assets/icons/WhiteAddBox.svg';
 import Image from 'next/image';
 
 type MouseEventHandler<T = Element> = EventHandler<MouseEvent<T>>;
@@ -12,7 +12,7 @@ interface ButtonProps {
 function InvitationButton({ onClick }: ButtonProps) {
   return (
     <button className={S.container} onClick={onClick}>
-      <Image src={AddButtonImg} width={16} height={16} alt="플러스 모양" />
+      <Image src={WhiteAddButtonImg} width={16} height={16} alt="플러스 모양" />
       초대하기
     </button>
   );
